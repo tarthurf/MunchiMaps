41c4ef4e5de52d4ec52694f0aafe8f39e64158f6
Swedish <tarthurf@yahoo.com>
2020-02-12 09:07:40 -0800

    Added ternary operators to check for null or blank addresses and skip over card creation for that index

--- script.js
+++ script.js
@@ -22,12 +22,16 @@ $(document).ready(function() {
 
         $.ajax(settings).done(function(response) {
             for (let index = 0; index < 7; index++) {
-                dispArray[index] = response.businesses[index];
+                const addressCheck = response.businesses[index].location.address1;
+                console.log(addressCheck);
+                if (addressCheck === null || addressCheck === "") continue;
+                if (addressCheck !== null || addressCheck !== "") dispArray[index] = response.businesses[index];
             }
             localStorage.setItem("dispArray", JSON.stringify(dispArray));
             var item = JSON.parse(localStorage.getItem("dispArray"));
             console.log(item);
             for (index = 0; index < item.length; index++) {
+                if (item[index] === null) continue;
                 makeCards(item[index], index);
             }
         });
@@ -90,6 +94,7 @@ $(document).ready(function() {
         });
         var content = $("<div>", { class: "content comforataa" });
         var busName = $("<div>", {
+            class: "subtitle",
             id: "businessName",
             text: obj.name
         });
